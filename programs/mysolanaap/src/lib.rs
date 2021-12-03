4a03b95df79681a50a5e9eaaa6e819f9457ff271
hakidehari <hakidehari@outlook.com>
2021-12-03 14:53:12 -0600

    added more details from tutorial

--- programs/mysolanaap/src/lib.rs
+++ programs/mysolanaap/src/lib.rs
@@ -1,43 +1,45 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6wwNtucDDkME38cjKyYC5EANpWVRmUKKf5s9PwjWHbo");
 
 #[program]
 mod mysolanaap {
     use super::*;
 
-    pub fn create(ctx: Context<Create>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.count = 0;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+    pub fn update(ctx: Context<Update>, data: String) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
-        base_account.count += 1;
+        let copy = data.clone();
+        base_account.data = data;
+        base_account.data_list.push(copy);
         Ok(())
     }
 }
 
-//Transaction instructions
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer = user, space = 16 + 16)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 64)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-//Transaction instructions
 #[derive(Accounts)]
-pub struct Increment<'info> {
+pub struct Update<'info> {
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
 }
 
-//an account that goes inside a transaction instruction
 #[account]
 pub struct BaseAccount {
-    pub count: u64,
+    pub data: String,
+    pub data_list: Vec<String>,
 }
--- tests/mysolanaap.js
+++ tests/mysolanaap.js
@@ -1,14 +1,43 @@
-const anchor = require('@project-serum/anchor');
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe('mysolanaap', () => {
+describe("Mysolanaap", () => {
+	  const provider = anchor.Provider.env();
+	  anchor.setProvider(provider);
+	  const program = anchor.workspace.Mysolanaap;
+	  it("It initializes the account", async () => {
+		      const baseAccount = anchor.web3.Keypair.generate();
+		      await program.rpc.initialize("Hello World", {
+			            accounts: {
+					            baseAccount: baseAccount.publicKey,
+					            user: provider.wallet.publicKey,
+					            systemProgram: SystemProgram.programId,
+					          },
+			            signers: [baseAccount],
+			          });
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+		      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+		      console.log('Data: ', account.data);
+		      assert.ok(account.data === "Hello World");
+		      _baseAccount = baseAccount;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Mysolanaap;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
+		    });
+
+	  it("Updates a previously created account", async () => {
+		      const baseAccount = _baseAccount;
+
+		      await program.rpc.update("Some new data", {
+			            accounts: {
+					            baseAccount: baseAccount.publicKey,
+					          },
+			          });
+
+		      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+		      console.log('Updated data: ', account.data);
+		      assert.ok(account.data === "Some new data");
+		      console.log('all account data:', account)
+		      console.log('All data: ', account.dataList);
+		      assert.ok(account.dataList.length === 2);
+		    });
 });
